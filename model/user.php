5391b37f6b6cfadc8403a60ff37bf3c7cac0e1e8
Beanz2003 <vothanhdat18052003@gmail.com>
2024-11-10 01:15:31 +0700

    feat: profile user

--- model/user.php
+++ model/user.php
@@ -122,4 +122,17 @@ class User extends Db
         $result = $sql->get_result();
         return $result->num_rows > 0; // Trả về true nếu tồn tại, false nếu không
     }
+    public function getProfileUser($userId) {
+        $sql = self::$connection->prepare("SELECT user_id, name, email, phone, address, username FROM user WHERE user_id = ?");
+        if (!$sql) {
+            die('Error preparing SQL: ' . self::$connection->error); 
+        }
+        $sql->bind_param("i", $userId);
+        $sql->execute();
+        $result = $sql->get_result();
+        if ($result->num_rows === 0) {
+            return null; 
+        }
+        return $result->fetch_assoc();
+    }
 }
