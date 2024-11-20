8d380f6d9a2ed917862a0c20c5f75d6e4ebe3512
NguyenQuangTruong03 <21211tt2568@mail.tdc.edu.vn>
2024-11-20 17:27:13 +0700

    create function fillter product and search product

--- model/product.php
+++ model/product.php
@@ -1,6 +1,43 @@
 <?php
 class Product extends Db
 {
+    public function searchProducts($searchTerm) {
+        $sql = "SELECT p.product_id, p.name, p.description, p.price, p.type, pi.image_url 
+        FROM product p LEFT JOIN product_image pi 
+        ON p.product_id = pi.product_id AND pi.is_main = 1 
+        WHERE p.name LIKE ? OR p.description LIKE ? OR p.type LIKE ?"; 
+        $stmt = self::$connection->prepare($sql);
+        $searchTerm = "%$searchTerm%"; 
+        $stmt->bind_param("sss", $searchTerm, $searchTerm, $searchTerm); $stmt->execute(); 
+        return $stmt->get_result()->fetch_all(MYSQLI_ASSOC); 
+    }
+    public function getAllColors(){
+        $sql = self::$connection->prepare("SELECT * FROM color");
+        $sql->execute();
+        $result = $sql->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+    public function getAllBrands(){
+        $sql = self::$connection->prepare("SELECT * FROM brand");
+        $sql->execute();
+        $result = $sql->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+    public function fillterProduct($brand_id, $color_name) {
+        $sql = " 
+        SELECT p.product_id, p.name AS name, p.price, pi.image_url, c.name AS color_name 
+        FROM product p 
+        JOIN product_color pc ON p.product_id = pc.product_id 
+        JOIN color c ON pc.color_id = c.color_id 
+        LEFT JOIN product_image pi ON p.product_id = pi.product_id AND pi.is_main = 1 
+        WHERE p.brand_id = ? AND c.name = ? 
+        ";
+        $stmt = self::$connection->prepare($sql);
+        $stmt->bind_param("is", $brand_id, $color_name);
+        $stmt->execute();
+        return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+    }
+    
     public function getSizeId($product_id)
     {
         $sql = "SELECT size_id FROM product_size WHERE product_id = ?";
