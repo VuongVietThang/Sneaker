558befef0a54e43c72467415077eb7b2444293d0
VietThang <21211tt3051@mail.tdc.edu.vn>
2024-11-22 23:48:19 +0700

    update

--- admin/header.php
+++ admin/header.php
@@ -5,13 +5,8 @@ require '../model/banner.php';
 require '../model/user.php';
 require '../model/brand.php';
 require '../model/faq.php';
-<<<<<<< HEAD
-require '../model/statistical.php';
-require '../model/encryption_helpers.php';
-=======
 require '../model/order.php';
 require '../model/contact.php';
->>>>>>> main
 session_start();
 if (!isset($_SESSION['user']) || !$_SESSION['user']['admin_id']) {
   header("Location: ../admin/404.php");
--- admin/index.php
+++ admin/index.php
@@ -8,51 +8,9 @@ include 'statistical_chart.php';
 <!-- partial -->
 <div class="container-fluid page-body-wrapper">
 
-<<<<<<< HEAD
-  <nav class="sidebar sidebar-offcanvas" id="sidebar">
-    <ul class="nav">
-      <li class="nav-item">
-        <a class="nav-link" href="index.php">
-          <i class="fas fa-th-large menu-icon"></i> <!-- Biểu tượng Dashboard -->
-          <span class="menu-title">Dashboard</span>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="quanlysanpham.php">
-          <i class="fas fa-box menu-icon"></i> <!-- Biểu tượng Quản lý sản phẩm -->
-          <span class="menu-title">Quản lý sản phẩm</span>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="quanlydonhang.php">
-          <i class="fas fa-shopping-cart menu-icon"></i> <!-- Biểu tượng giỏ hàng cho đơn hàng -->
-          <span class="menu-title">Quản lý đơn hàng</span>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="quanlybanner.php">
-          <i class="fas fa-image menu-icon"></i> <!-- Biểu tượng giỏ hàng cho đơn hàng -->
-          <span class="menu-title">Quản lý banner</span>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="quanlyuser.php">
-          <i class="fas fa-user menu-icon"></i> <!-- Biểu tượng giỏ hàng cho đơn hàng -->
-          <span class="menu-title">Quản lý user</span>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="quanlybrand.php">
-          <i class="fas fa-list menu-icon"></i> <!-- Biểu tượng giỏ hàng cho đơn hàng -->
-          <span class="menu-title">Quản lý brand</span>
-        </a>
-      </li>
-    </ul>
-=======
     <?php include 'sidebar.php' ?>
       <!-- partial -->
       <div class="main-panel">
->>>>>>> main
 
   </nav>
 
